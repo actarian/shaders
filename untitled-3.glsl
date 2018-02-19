4df9195a0f58ef674ed0bbf46957d97b15a8c344
actarian <lzampetti@gmail.com>
2018-02-19 17:53:21 +0100

    shaders

--- untitled-3.glsl
+++ untitled-3.glsl
@@ -1,21 +1,62 @@
 precision mediump float;
 
+/***   u n i f o r m s   ***/
+
 uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
 uniform sampler2D u_texture_0;
 uniform vec3 u_color;
 
-/* Shape polygon 2d return float */
+/***   c o n s t a n t s   ***/
+
+#define st vec2((gl_FragCoord.x / u_resolution.x - 0.5) * -1.0, ((gl_FragCoord.y / u_resolution.y * u_resolution.y / u_resolution.x) + ((u_resolution.x - u_resolution.y) / u_resolution.x / 2.0) - 0.5) * 1.0)
+#define mx vec2((u_mouse.x / u_resolution.x - 0.5) * 1.0, ((u_mouse.y / u_resolution.y * u_resolution.y / u_resolution.x) + ((u_resolution.x - u_resolution.y) / u_resolution.x / 2.0) - 0.5) * -1.0)
+
 #define PI				3.14159265359
 #define TWO_PI			6.28318530718
+
+#define RED             vec3(1.0, 0.0, 0.0)
+#define BLUE            vec3(0.0, 0.0, 1.0)
+#define YELLOW          vec3(1.0, 1.0, 0.0)
+
+/*
+vec2 getSt() {
+	vec2 st = gl_FragCoord.xy / u_resolution.xy;
+    // correct aspect ratio
+	st.y *= u_resolution.y / u_resolution.x;
+	st.y += (u_resolution.x - u_resolution.y) / u_resolution.x / 2.0;
+    // centering
+    st -= 0.5;
+    st *= vec2(-1.0, 1.0);
+	return st;
+}
+
+vec2 getMx() {
+	vec2 mx = u_mouse / u_resolution.xy;
+    // correct aspect ratio
+	mx.y *= u_resolution.y / u_resolution.x;
+	mx.y += (u_resolution.x - u_resolution.y) / u_resolution.x / 2.0;
+    // centering
+    mx -= 0.5;
+    mx *= vec2(1.0, -1.0);
+	return mx;
+}
+
+#define st getSt()
+#define mx getMx()
+*/
+
+/***   m a t h   ***/
 
 mat2 rotate2d(float a){
     return mat2(cos(a), -sin(a), sin(a), cos(a));
 }
 
-float plot(vec2 st, float pct) {
-  return smoothstep(pct - 0.002, pct, st.y) - smoothstep(pct, pct + 0.002, st.y);
+/***   s h a p e s   ***/
+
+float plot(vec2 p, float pct) {
+  return smoothstep(pct - 0.002, pct, p.y) - smoothstep(pct, pct + 0.002, p.y);
 }
 
 float polygon(vec2 p, int sides) {
@@ -50,6 +91,8 @@ vec2 move(vec2 p, float d) {
     );
 }
 
+/***   e a s i n g s   ***/
+
 // t: current time, b: begInnIng value, c: change In value, d: duration	
 float easeInOutQuad(float t, float b, float c, float d) {
     if ((t /= d / 2.0) < 1.0) {
@@ -74,16 +117,24 @@ float easeOutBounce(float t) {
     }
 }
 
-struct Stepper { float time; float pow; };
-Stepper step = Stepper(0.0, 0.0);
-void totalTime(float t, float d) { step.time = mod(u_time + d, t); }
+/***   o b j e c t s   ***/
+
+struct Object { float distance; vec3 color; };
+Object object = Object(1000000.0, vec3(0.0));
+
+/***   a n i m a t i o n   ***/
+
+struct Animation { float time; float pow; };
+Animation animation = Animation(0.0, 0.0);
+void totalTime(float t, float d) { animation.time = mod(u_time + d, t); }
 void totalTime(float t) { totalTime(t, 0.0); }
 bool between(float duration) {
-    float p = step.time / duration;
-    step.pow = p;
-    step.time -= duration;
+    float p = animation.time / duration;
+    animation.pow = p;
+    animation.time -= duration;
     return (p >= 0.0 && p <= 1.0);
 }
+
 float tween(float a, float b, float v) { return a + (b - a) * v; }
 vec2 tween(vec2 a, vec2 b, float v) { return a + (b - a) * v; }
 vec3 tween(vec3 a, vec3 b, float v) { return a + (b - a) * v; }
@@ -92,90 +143,128 @@ vec4 tween(vec4 a, vec4 b, float v) { return a + (b - a) * v; }
 vec3 repeat(vec3 p, vec3 r) { return mod(p, r) -0.5 * r; }
 vec2 repeat(vec2 p, vec2 r) { return mod(p, r) -0.5 * r; }
 
-vec2 tile(vec2 p, float zoom) {
-    p *= zoom;
-    return fract(p);
-}
+vec2 tile(vec2 p, float zoom) { p *= zoom; return fract(p); }
 
-vec3 render(vec2 st, vec3 color, float diff) {
+void animate(vec2 p, float diff) {
+    float v; // assume values between 0.0 && 1.0
+    vec3 c = vec3(0.0); // color
 
-    vec3 red = vec3(1.0, 0.0, 0.0);
-    vec3 blue = vec3(0.0, 0.0, 1.0);
-    vec3 yellow = vec3(1.0, 1.0, 0.0);
-    
-    float v;
-    vec2 p;
-    vec3 c = vec3(0.0);
-
-    st *= 1.5;
-    // st = tile(st, 4.0);
+    p *= 1.5;
+    // p = tile(p, 4.0);
     
     /*
-    st *= 10.0;
-    st = repeat(st, vec2(2.0, 2.0));
+    p *= 10.0;
+    p = repeat(p, vec2(2.0, 2.0));
     */
 
     totalTime(3.5, diff);
     if (between(1.0)) {
-        v = easeInOutQuad(step.pow);
-        p = st + vec2(0.5 - v, 0.0);
-        c = tween(red, blue, v);
+        v = easeInOutQuad(animation.pow);
+        p = p + vec2(0.5 - v, 0.0);
+        object.color = tween(RED, BLUE, v);
     }
 
     if (between(1.0)) {
-        v = easeOutBounce(step.pow);
-        p = st + vec2(-0.5 + v, 0.0);
-        c = tween(blue, red, v);
+        v = easeOutBounce(animation.pow);
+        p = p + vec2(-0.5 + v, 0.0);
+        object.color = tween(BLUE, RED, v);
     }
 
     if (between(0.5)) {
-        v = easeInOutQuad(step.pow);
-        p = st + vec2(0.5 * cos(v * PI), 0.5 * sin(v * PI));
-        c = tween(red, yellow, v);
+        v = easeInOutQuad(animation.pow);
+        p = p + vec2(0.5 * cos(v * PI), 0.5 * sin(v * PI));
+        object.color = tween(RED, YELLOW, v);
     }
     
     if (between(1.0)) {
-        v = easeOutBounce(step.pow);
-        p = st + vec2(-0.5 + v, 0.0);
-        c = tween(yellow, red, v);
+        v = easeOutBounce(animation.pow);
+        p = p + vec2(-0.5 + v, 0.0);
+        object.color = tween(YELLOW, RED, v);
     }
 
-    return mix(color, c, circle(p, 0.005));
-    // return mix(color, c, rect(p, vec2(0.005)));
+    object.distance = circle(p, 0.005);
+    // object.distance = rect(p, vec2(0.005));
 }
 
-vec2 getSt() {
-	vec2 st = gl_FragCoord.xy / u_resolution.xy;
-    // correct aspect ratio
-	st.y *= u_resolution.y / u_resolution.x;
-	st.y += (u_resolution.x - u_resolution.y) / u_resolution.x / 2.0;
-    // centering
-    st -= 0.5;
-    st *= vec2(-1.0, 1.0);
-	return st;
+const int MAX_MARCHING_STEPS = 255;
+const float MIN_DIST = 0.0;
+const float MAX_DIST = 100.0;
+const float EPSILON = 0.0001;
+
+/**
+ * Signed distance function for a sphere centered at the origin with radius 1.0;
+ */
+float sphereSDF(vec3 samplePoint) {
+    return length(samplePoint) - 1.0;
 }
 
-vec2 getMx() {
-	vec2 mx = u_mouse / u_resolution.xy;
-    // correct aspect ratio
-	mx.y *= u_resolution.y / u_resolution.x;
-	mx.y += (u_resolution.x - u_resolution.y) / u_resolution.x / 2.0;
-    // centering
-    mx -= 0.5;
-    mx *= vec2(1.0, -1.0);
-	return mx;
+/**
+ * Signed distance function describing the scene.
+ * 
+ * Absolute value of the return value indicates the distance to the surface.
+ * Sign indicates whether the point is inside or outside the surface,
+ * negative indicating inside.
+ */
+float sceneSDF(vec3 samplePoint) {
+    return sphereSDF(samplePoint);
+}
+
+/**
+ * Return the shortest distance from the eyepoint to the scene surface along
+ * the marching direction. If no part of the surface is found between start and end,
+ * return end.
+ * 
+ * eye: the eye point, acting as the origin of the ray
+ * marchingDirection: the normalized direction to march in
+ * start: the starting distance away from the eye
+ * end: the max distance away from the ey to march before giving up
+ */
+float shortestDistanceToSurface(vec3 eye, vec3 marchingDirection, float start, float end) {
+    float depth = start;
+    for (int i = 0; i < MAX_MARCHING_STEPS; i++) {
+        float dist = sceneSDF(eye + depth * marchingDirection);
+        if (dist < EPSILON) {
+			return depth;
+        }
+        depth += dist;
+        if (depth >= end) {
+            return end;
+        }
+    }
+    return end;
+}
+            
+/**
+ * Return the normalized direction to march in from the eye point for a single pixel.
+ * 
+ * fieldOfView: vertical field of view in degrees
+ * p: the x,y coordinate of the pixel in the output image
+ * size: resolution of the output image
+ */
+vec3 rayDirection(float fieldOfView, vec2 p, vec2 size) {
+    vec2 xy = p - size / 2.0;
+    float z = size.y / tan(radians(fieldOfView) / 2.0);
+    return normalize(vec3(xy, -z));
 }
 
 void main() {
-    vec2 st = getSt();
-    vec2 mx = getMx();
-
-    vec3 color = vec3(1.0, 1.0, 1.0); // u_color;
+    vec3 color = vec3(1.0, 1.0, 1.0);
     
-    // color = mix(vec3(0.0), color, circle(st + mx, 0.005));
+    animate(st * rotate2d(u_time * 0.1), 0.0);
+    color = mix(color, object.color, object.distance); // drawing object on color
 
-    color = render(st * rotate2d(u_time * 0.1), color, 0.0);
-    color = render(st * rotate2d(-u_time * 0.1), color, 0.4);
+    animate(st * rotate2d(-u_time * 0.1), 0.4);
+    color = mix(color, object.color, object.distance); // drawing object on color
 
+    vec3 dir = rayDirection(120.0, gl_FragCoord.xy, u_resolution.xy);
+    vec3 eye = vec3(0.0, 0.0, 5.0);
+    float dist = shortestDistanceToSurface(eye, dir, MIN_DIST, MAX_DIST);
+
+    if (dist > MAX_DIST - EPSILON) {
+        // Didn't hit anything
+        gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);
+		return;
+    }
+    
     gl_FragColor = vec4(color, 1.0);
 }
---
+++ wave.glsl
@@ -0,0 +1,70 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform float u_time;  
+
+float Band(float t, float start, float end, float blur){
+    float step1 = smoothstep(start - blur, start + blur, t);
+    float step2 = smoothstep(end + blur, end - blur, t);
+    float band = step1 * step2;
+    return band;
+}
+
+float remap01(float a, float b, float t) {
+    return (t - a) / (b - a);
+}
+
+float remap(float a, float b, float c, float d, float t) {
+    return remap01(a, b, t) * (d - c) + c;
+}
+
+float Rect(vec2 uv, float left, float right, float bottom, float top, float blur){
+    float band1 = Band(uv.x, left, right, blur);
+    float band2 = Band(uv.y, bottom, top, blur);
+    float rect = band1 * band2;
+    
+    return rect;
+}
+
+void main(){
+	vec2 uv = gl_FragCoord.xy / u_resolution.xy;
+    uv -= 0.5;
+    
+    float t = 1.5 + u_time * 3.0;
+
+    float x = uv.x;
+    float magic = sin(t + x * 8.) * 0.1;
+    float y = uv.y - magic;
+    
+    // x += x * sin(iTime * 7.) * 0.2;
+    // 1x += y * cos(iTime * 7.) * 0.2;
+    // y += x * cos(iTime * 7.) * 0.2;
+    
+    
+    y += y - magic;
+    
+    // float band = Band(x, -0.2, 0.2, 0.001);
+    // float band2 = Band(y, -0.2, 0.2, 0.001);
+    // float rect = band * band2;
+    
+    float blur = remap(0.5, -0.5, 0.001, 0.5, x);
+    blur = pow(blur * 2.5, 2.5);
+    
+    float rect = Rect(vec2(x, y), -0.5, 0.5, -0.05, 0.05, blur);
+      
+    
+    vec4 baseColor = vec4(0.4 + sin(u_time * 0.6 + x * y) , 0.2, 0.9, 1.0);
+    
+    vec3 col = vec3(0.9, 0.8, 0.2) * vec3(rect);
+    
+    float rect2 = Rect(vec2(x, y), -0.3, 0.5, -0.02, 0.0, blur);
+    
+    vec3 col2 = vec3(0.2, 0.8, 0.8) * vec3(rect);
+    
+	gl_FragColor = vec4(vec3(col2 + col),1.0) + baseColor;
+    
+}
+
+
