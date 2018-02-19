35a648da5f3eeca35a873fb3b47c400971ba0cc5
Luca Zampetti <lzampetti@gmail.com>
2018-02-20 00:38:14 +0100

    shaders

--- untitled-3.glsl
+++ untitled-3.glsl
@@ -13,8 +13,9 @@ uniform vec3 u_color;
 #define st vec2((gl_FragCoord.x / u_resolution.x - 0.5) * -1.0, ((gl_FragCoord.y / u_resolution.y * u_resolution.y / u_resolution.x) + ((u_resolution.x - u_resolution.y) / u_resolution.x / 2.0) - 0.5) * 1.0)
 #define mx vec2((u_mouse.x / u_resolution.x - 0.5) * 1.0, ((u_mouse.y / u_resolution.y * u_resolution.y / u_resolution.x) + ((u_resolution.x - u_resolution.y) / u_resolution.x / 2.0) - 0.5) * -1.0)
 
-#define PI				3.14159265359
-#define TWO_PI			6.28318530718
+#define PI_TWO			1.570796326794897
+#define PI				3.141592653589793
+#define TWO_PI			6.283185307179586
 
 #define RED             vec3(1.0, 0.0, 0.0)
 #define BLUE            vec3(0.0, 0.0, 1.0)
@@ -60,7 +61,7 @@ float plot(vec2 p, float pct) {
 }
 
 float polygon(vec2 p, int sides) {
-    p -= 0.5;
+    // p -= 0.5;
     // Angle and radius from the current pixel
     float a = atan(p.x, p.y) + PI;
     float r = TWO_PI / float(sides);
@@ -69,14 +70,14 @@ float polygon(vec2 p, int sides) {
     return 1.0 - smoothstep(0.1, 0.1001, d);
 }
 
-float rect(vec2 p, vec2 size){
-	size = 0.25 - size * 0.25;
-    vec2 uv = step(size, p * (1.0 - p));
-	return uv.x * uv.y;
+float rect(vec2 p, vec2 size) {
+    vec2 s2 = size / -2.0;
+    vec2 bl = smoothstep(vec2(s2 - 0.001), vec2(s2), p);
+    vec2 tr = smoothstep(vec2(s2 + 1.0), vec2(s2 + 1.0 + 0.001), 1.0 - p);
+    return bl.x * bl.y * tr.x * tr.y;
 }
 
 float circle(vec2 p, float r) {
-    p += 0.0;
     return 1.0 - smoothstep(
         r - (r * 0.01),
         r + (r * 0.01),
@@ -93,28 +94,36 @@ vec2 move(vec2 p, float d) {
 
 /***   e a s i n g s   ***/
 
-// t: current time, b: begInnIng value, c: change In value, d: duration	
-float easeInOutQuad(float t, float b, float c, float d) {
-    if ((t /= d / 2.0) < 1.0) {
-        return c / 2.0 * t * t + b;
+// Quadratic
+float easeInQuad(float t) {
+    return t * t;
+}
+float easeOutQuad(float t) {
+    return -1.0 * t * (t - 2.0);
+}
+float easeInOutQuad(float t) {
+    if ((t / 2.0) < 1.0) return 0.5 * t * t;
+    return -0.5 * ((--t) * (t - 2.0) - 1.0);
+}
+
+// Bounce
+float easeOutBounce(float t) {
+    if (t < (1.0 / 2.75)) {
+        return (7.5625 * t * t);
+    } else if (t < (2.0 / 2.75)) {
+        return (7.5625 * (t -= (1.5 / 2.75)) * t + 0.75);
+    } else if (t < (2.5 / 2.75)) {
+        return (7.5625 * (t -= (2.25 / 2.75)) * t + 0.9375);
     } else {
-        return -c / 2.0 * ((--t) * (t - 2.0) - 1.0) + b;
+        return (7.5625 * (t -= (2.625 / 2.75)) * t + 0.984375);
     }
 }
-float easeInOutQuad(float t) { return easeInOutQuad(t, 0.0, 1.0, 1.0); }
-float easeOutBounce(float t) {
-    float b = 0.0;
-    float c = 1.0; 
-    float d = 1.0;
-    if ((t /= d) < (1.0/2.75)) {
-        return c * (7.5625 * t * t) + b;
-    } else if (t < (2.0 / 2.75)) {
-        return c * (7.5625 * (t -= (1.5 / 2.75)) * t + 0.75) + b;
-    } else if (t < (2.5 / 2.75)) {
-        return c * (7.5625 * (t -= (2.25 / 2.75)) * t + 0.9375) + b;
-    } else {
-        return c * (7.5625 * (t -= (2.625 / 2.75)) * t + 0.984375) + b;
-    }
+float easeInBounce(float t) {
+    return 1.0 - easeOutBounce(1.0 - t);
+}
+float easeInOutBounce(float t) {
+    if (t < 0.5) return easeInBounce(t * 2.0) * 0.5;
+    else return easeOutBounce(t * 2.0 - 1.0) * 0.5 + 0.5;
 }
 
 /***   o b j e c t s   ***/
@@ -157,12 +166,7 @@ void animate(vec2 p, float diff) {
     p = repeat(p, vec2(2.0, 2.0));
     */
 
-    totalTime(3.5, diff);
-    if (between(1.0)) {
-        v = easeInOutQuad(animation.pow);
-        p = p + vec2(0.5 - v, 0.0);
-        object.color = tween(RED, BLUE, v);
-    }
+    totalTime(5.0, diff);
 
     if (between(1.0)) {
         v = easeOutBounce(animation.pow);
@@ -170,55 +174,40 @@ void animate(vec2 p, float diff) {
         object.color = tween(BLUE, RED, v);
     }
 
-    if (between(0.5)) {
-        v = easeInOutQuad(animation.pow);
-        p = p + vec2(0.5 * cos(v * PI), 0.5 * sin(v * PI));
+    if (between(3.0)) {
+        v = easeOutQuad(animation.pow);
+        p = p + vec2(0.5 * cos(v * 2.0 * TWO_PI), 0.5 * sin(v * 2.0 * TWO_PI));
         object.color = tween(RED, YELLOW, v);
     }
     
     if (between(1.0)) {
         v = easeOutBounce(animation.pow);
-        p = p + vec2(-0.5 + v, 0.0);
-        object.color = tween(YELLOW, RED, v);
+        p = p + vec2(0.5 - v, 0.0);
+        object.color = tween(YELLOW, BLUE, v);
     }
 
-    object.distance = circle(p, 0.005);
-    // object.distance = rect(p, vec2(0.005));
+    // p *= rotate2d(u_time);
+
+    // object.distance = circle(p, 0.01);
+    object.distance = rect(p, vec2(0.1));
+    // object.distance = polygon(p, 4);
+    
 }
 
+/*
 const int MAX_MARCHING_STEPS = 255;
 const float MIN_DIST = 0.0;
 const float MAX_DIST = 100.0;
 const float EPSILON = 0.0001;
 
-/**
- * Signed distance function for a sphere centered at the origin with radius 1.0;
- */
 float sphereSDF(vec3 samplePoint) {
     return length(samplePoint) - 1.0;
 }
 
-/**
- * Signed distance function describing the scene.
- * 
- * Absolute value of the return value indicates the distance to the surface.
- * Sign indicates whether the point is inside or outside the surface,
- * negative indicating inside.
- */
 float sceneSDF(vec3 samplePoint) {
     return sphereSDF(samplePoint);
 }
 
-/**
- * Return the shortest distance from the eyepoint to the scene surface along
- * the marching direction. If no part of the surface is found between start and end,
- * return end.
- * 
- * eye: the eye point, acting as the origin of the ray
- * marchingDirection: the normalized direction to march in
- * start: the starting distance away from the eye
- * end: the max distance away from the ey to march before giving up
- */
 float shortestDistanceToSurface(vec3 eye, vec3 marchingDirection, float start, float end) {
     float depth = start;
     for (int i = 0; i < MAX_MARCHING_STEPS; i++) {
@@ -233,29 +222,26 @@ float shortestDistanceToSurface(vec3 eye, vec3 marchingDirection, float start, f
     }
     return end;
 }
-            
-/**
- * Return the normalized direction to march in from the eye point for a single pixel.
- * 
- * fieldOfView: vertical field of view in degrees
- * p: the x,y coordinate of the pixel in the output image
- * size: resolution of the output image
- */
+
 vec3 rayDirection(float fieldOfView, vec2 p, vec2 size) {
     vec2 xy = p - size / 2.0;
     float z = size.y / tan(radians(fieldOfView) / 2.0);
     return normalize(vec3(xy, -z));
 }
+*/
 
 void main() {
     vec3 color = vec3(1.0, 1.0, 1.0);
     
-    animate(st * rotate2d(u_time * 0.1), 0.0);
+    animate(st, 0.0);
     color = mix(color, object.color, object.distance); // drawing object on color
 
+/*
     animate(st * rotate2d(-u_time * 0.1), 0.4);
     color = mix(color, object.color, object.distance); // drawing object on color
+*/
 
+/*
     vec3 dir = rayDirection(120.0, gl_FragCoord.xy, u_resolution.xy);
     vec3 eye = vec3(0.0, 0.0, 5.0);
     float dist = shortestDistanceToSurface(eye, dir, MIN_DIST, MAX_DIST);
@@ -265,6 +251,7 @@ void main() {
         gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);
 		return;
     }
-    
+*/
+
     gl_FragColor = vec4(color, 1.0);
 }
