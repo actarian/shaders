050e31991cec22117621fc97025dfe33e0f92c57
Luca Zampetti <lzampetti@gmail.com>
2018-03-01 09:28:14 +0100

    shaders

--- stagger/stagger.1.glsl
+++ stagger/stagger.1.glsl
@@ -44,15 +44,18 @@ vec2 coord(in vec2 p) {
     p *= vec2(-1.0, 1.0);
 	return p;
 }
-#define rx 1.0 / min(u_resolution.x, u_resolution.y)
-#define uv gl_FragCoord.xy / u_resolution.xy
+vec2 getuv(in vec2 p) {
+	p = p / u_resolution.xy;
+	return p;
+}
 #define st coord(gl_FragCoord.xy)
+#define uv getuv(gl_FragCoord.xy)
 #define mx coord(u_mouse)
+#define rx 1.0 / min(u_resolution.x, u_resolution.y)
 vec2 pos(in float x, in float y) { return st + vec2(x * rx, y * rx); }
 vec2 pos(in float x) { return pos(x, x); }
 vec2 pos(in vec2 p) { return pos(p.x, p.y); }
-float pix(in float x) { return x * rx; }
-vec2 pix(in float x, in float y) { return vec2(x * rx, y * rx); }
+
 /***   m a t h   ***/
 
 mat2 rotate2d(float a){
@@ -68,9 +71,9 @@ Object object = Object(1000000.0, vec3(0.0));
 
 struct Animation { float time; float pow; };
 Animation animation = Animation(0.0, 0.0);
-void totalTime(in float t, in float d) { animation.time = mod(u_time + d, t); }
-void totalTime(in float t) { totalTime(t, 0.0); }
-bool between(in float duration) {
+void totalTime(float t, float d) { animation.time = mod(u_time + d, t); }
+void totalTime(float t) { totalTime(t, 0.0); }
+bool between(float duration) {
     float p = animation.time / duration;
     animation.pow = p;
     animation.time -= duration;
@@ -111,188 +114,68 @@ float easeElasticOut(in float t) {
 
 /***   s h a p e s   ***/
 
-float plot(in vec2 p, in float t, in float a) {
-    p *= rotate2d(a);
-    return 1.0 - smoothstep(t / 2.0 - rx, t / 2.0 + rx, abs(p.x));
-}
-float plot(in vec2 p, in float t) { return plot (p, t, 0.0); }
-float plot(in vec2 p) { return plot (p, 1.0, 0.0); }
-
 float line(in vec2 a, in vec2 b, float size) {
     vec2 ba = a - b;
-    float d = clamp(dot(a, ba) / dot(ba, ba), 0.0, 1.0);
-    d = length(a - ba * d);
-    return smoothstep(size + rx, size - rx, d);
+    float f = clamp(dot(a, ba) / dot(ba, ba), 0.0, 1.0);
+    f = length(a - ba * f);
+    return smoothstep(size * rx + rx, size * rx - rx, f);
 }
 
 float circle(in vec2 p, in float size) {
     float d = length(p) * 2.0;
-    return 1.0 - smoothstep(size - rx, size + rx, d);
+    return 1.0 - smoothstep(size * rx - rx, size * rx + rx, d);
 }
 float circle(in vec2 p, in float size, float t) {
-    float d = length(abs(p)) - size / 2.0;
-    return 1.0 - smoothstep(t - rx, t + rx, abs(d));
+    float d = length(abs(p)) - size * rx / 2.0;
+    return 1.0 - smoothstep(t * rx - rx, t * rx + rx, abs(d));
 }
 
 float rect(in vec2 p, in vec2 size) {
-    size /= 2.0;
+    size *= rx; size /= 2.0;
     float d = length(max(abs(p) -size, 0.0));
     return 1.0 - smoothstep(0.0, 0.0 + rx * 2.0, d);
 }
 float rect(in vec2 p, in vec2 size, in float t) {
-    size /= 2.0;
-    float d = length(max(abs(p), size - t) - size + t) - t * 2.0;
-    return 1.0 - smoothstep(t - rx, t + rx, abs(d));
+    size *= rx; size /= 2.0; float t2 = t * rx / 2.0;
+    float d = length(max(abs(p), size - t * rx) - size + t * rx) - t * rx * 2.0;
+    return 1.0 - smoothstep(t * rx - rx, t * rx + rx, abs(d));
 }
 
 float roundrect(in vec2 p, in vec2 size, in float radius) {
-    radius *= 2.0; size /= 2.0;
+    radius *= rx * 2.0; size *= rx; size /= 2.0;
     float d = length(max(abs(p) -size + radius, 0.0)) - radius;
     return 1.0 - smoothstep(0.0, 0.0 + rx * 2.0, d);
 }
 float roundrect(in vec2 p, in vec2 size, in float radius, in float t) {
-    radius *= 2.0; size /= 2.0; size -= radius;
+    radius *= rx * 2.0; size *= rx; size /= 2.0; size -= radius;
     float d = length(max(abs(p), size) - size) - radius;
-    return 1.0 - smoothstep(t - rx, t + rx, abs(d));
+    return 1.0 - smoothstep(t * rx - rx, t * rx + rx, abs(d));
 }
 
-float poly(in vec2 p, in float size, in int sides) {
+float polygon(in vec2 p, in float size, in int sides) {
     float a = atan(p.x, p.y) + PI;
     float r = TWO_PI / float(sides);
     float d = cos(floor(0.5 + a / r) * r - a) * length(max(abs(p) * 1.0, 0.0));
-    return 1.0 - smoothstep(size / 2.0 - rx, size / 2.0 + rx, d);
+    return 1.0 - smoothstep(size * rx / 2.0 - rx, size * rx / 2.0 + rx, d);
 }
 
-float poly(in vec2 p, in float size, in int sides, in float t) {
+float polygon(in vec2 p, in float size, in int sides, in float t) {
     float a = atan(p.x, p.y) + PI;
     float r = TWO_PI / float(sides);
-    float d = cos(floor(0.5 + a / r) * r - a) * length(max(abs(p) * 1.0, 0.0)) - size / 2.0;
-    return 1.0 - smoothstep(t - rx, t + rx, abs(d));
+    float d = cos(floor(0.5 + a / r) * r - a) * length(max(abs(p) * 1.0, 0.0)) - size * rx / 2.0;
+    return 1.0 - smoothstep(t * rx - rx, t * rx + rx, abs(d));
 }
 
-float grid(in float size) {
-    float d = 0.0;
-    d += plot(tile(st, size), pix(1.0));
-    d += plot(tile(st, size), pix(1.0), PI_TWO);
-    d *= 0.1;
-    vec2 p = tile(st, vec2(size * 5.0, size * 5.0));
-    float s = size / 10.0;
-    float g = 0.0;
-    g += line(p + vec2(-s, 0.0), p + vec2(s, 0.0), pix(1.0));
-    g += line(p + vec2(0.0, -s), p + vec2(0.0, s), pix(1.0));
-    return d + g;
+float plot(in vec2 p, in float t, in float a) {
+    p *= rotate2d(a);
+    return 1.0 - smoothstep(t / 2.0 * rx - rx, t / 2.0 * rx + rx, abs(p.x));
 }
-
-vec3 draw(in sampler2D t, in vec2 pos, in vec2 size) {
-    vec2 s = size / 1.0;
-    
-    return texture2D(t, uv * size - pos).rgb;
-}
-
-void animate(in float diff) {
-    vec3 c = vec3(0.0);
-
-    vec2 p = st;
-    
-    // p *= rotate2d(u_time);
-
-    float r = pix(250.0);
-
-    totalTime(10.0, diff);
-
-    float v; // assume values between 0.0 && 1.0
-    if (between(2.0)) {
-        v = easeBounceOut(animation.pow);
-        p += vec2(mix(-r, r, v), 0.0);
-        object.color = mix(YELLOW, WHITE, v);
-        object.distance = circle(p, pix(50.0), pix(2.0));
-    }
-
-    if (between(6.0)) {
-        v = easeElasticOut(animation.pow);
-        p = p + vec2(r * cos(v * 2.0 * PI), r * sin(v * 2.0 * PI));
-        object.color = mix(WHITE, GREEN, v);
-        object.distance = roundrect(p, pix(50.0, 50.0), pix(5.0), pix(2.0));
-    }
-    
-    if (between(2.0)) {
-        v = easeBounceOut(animation.pow);
-        p += vec2(mix(r, -r, v), 0.0);
-        object.color = mix(GREEN, YELLOW, v);
-        object.distance = poly(p, pix(50.0), 4, pix(2.0));
-    }
-
-    // object.distance = smoothstep(0.9 - rx, 0.9 + rx, object.distance);
-    // object.distance = circle(p, 30.0 + 20.0 * v);
-}
-
-void main() {
-    vec3 color = BLACK;
-    
-    color = mix(color, GREEN, 
-        grid(pix(50.0))
-    );
-    
-    animate(0.0);
-    color = mix(color, object.color, 
-        object.distance
-    );
-
-    color = mix(color, MAGENTA, 
-        roundrect((pos(-125.0, -125.0)) * rotate2d(-u_time), pix(100.0, 100.0), pix(10.0))
-    );
-
-    color = mix(color, MAGENTA, 
-        roundrect((pos(125.0, -125.0)) * rotate2d(u_time), pix(100.0, 100.0), pix(10.0), pix(1.0))
-    );
-
-    color = mix(color, MAGENTA, 
-        circle(pos(-125.0, 125.0), pix(30.0))
-    );
-
-    color = mix(color, MAGENTA, 
-        circle(pos(125.0, 125.0), pix(30.0), pix(1.0))
-    );
-  
-    color = mix(color, MAGENTA, 
-        rect(pos(-125.0, 225.0), pix(90.0 + sin(u_time) * 60.0, 90.0 + cos(u_time) * 60.0))
-    );
-
-    color = mix(color, MAGENTA, 
-        rect(pos(125.0, 225.0), pix(90.0 + cos(u_time) * 60.0, 90.0 + sin(u_time) * 60.0), pix(1.0))
-    );
-
-    color = mix(color, YELLOW, 
-        poly(pos(-125.0, -225.0), pix(30.0), 3)
-    );
-
-    color = mix(color, YELLOW, 
-        poly(pos(125.0, -225.0), pix(30.0), 3, pix(4.0))
-    );
-
-    color = mix(color, YELLOW, 
-        line(pos(50.0 * cos(u_time), 0.0), pos(150.0 * sin(u_time), 150.0), pix(5.0))
-    );
-
-    color = mix(color, YELLOW, 
-        line(pos(-50.0 * cos(u_time), 0.0), pos(-150.0 * sin(u_time), 150.0), pix(5.0))
-    );
-
-    color = mix(color, draw(u_texture_0, pos(0.0, 125.0), pix(150.0, 150.0)), 
-        roundrect(pos(0.0, 125.0), pix(150.0, 50.0), pix(5.0))
-        // rect(st, vec2(2.0, 2.0))
-    );
-
-    float c = 0.0; // cos(u_time * 60.0) * 0.05;
-
-    gl_FragColor = vec4(color + c * length(st), 1.0);
-}
-
-
+float plot(in vec2 p, in float t) { return plot (p, t, 0.0); }
+float plot(in vec2 p) { return plot (p, 1.0, 0.0); }
 
 /////////////////
 
-float _rect(vec2 p, vec2 size) {
+float rect_(vec2 p, vec2 size) {
     size *= rx; size /= -2.0;
     vec2 bl = smoothstep(vec2(size - 0.001), vec2(size), p);
     vec2 tr = smoothstep(vec2(size + 1.0), vec2(size + 1.0 + 0.001), 1.0 - p);
@@ -304,14 +187,14 @@ float _line(vec2 a, vec2 b, vec2 p, float size) {
     vec2 perp = vec2(ba.y, -ba.x);
     vec2 dir = a - p;
     float f = abs(dot(normalize(perp), dir));
-    return smoothstep(size + rx, size - rx, f);
+    return smoothstep(size * rx + rx, size * rx - rx, f);
 }
 
 float __line(vec2 p1, vec2 p2, vec2 p, float size) {
     float a = p1.y - p2.y;
     float b = p2.x - p1.x;
     float f = abs(a * p.x + b * p.y + p1.x * p2.y - p2.x * p1.y) / sqrt(a * a + b * b);
-    return smoothstep(size + rx, size - rx, f);
+    return smoothstep(size * rx + rx, size * rx - rx, f);
 }
 
 float ___liner(vec2 a, vec2 b, float t) {
@@ -320,4 +203,129 @@ float ___liner(vec2 a, vec2 b, float t) {
     vec2 size = vec2(distance(a, b) * u_resolution.x, t);
     p *= rotate2d(angle);
     return rect(p, size);
+}
+
+float grid(in float size) {
+    size *= rx;
+    float f = 0.0;
+    f += plot(tile(st, size), 1.0);
+    f += plot(tile(st, size), 1.0, PI_TWO);
+    f *= 0.1;
+    vec2 p = tile(st, vec2(size * 5.0, size * 5.0));
+    float s = size / 10.0;
+    float g = 0.0;
+    g += line(p + vec2(-s, 0.0), p + vec2(s, 0.0), 1.0);
+    g += line(p + vec2(0.0, -s), p + vec2(0.0, s), 1.0);
+    return f + g;
+}
+
+void animate(in float diff) {
+    vec3 c = vec3(0.0);
+
+    vec2 p = st;
+    
+    // p *= rotate2d(u_time);
+
+    float r = 250.0 * rx;
+
+    totalTime(10.0, diff);
+
+    float v; // assume values between 0.0 && 1.0
+    if (between(2.0)) {
+        v = easeBounceOut(animation.pow);
+        p += vec2(mix(-r, r, v), 0.0);
+        object.color = mix(YELLOW, WHITE, v);
+        object.distance = circle(p, 50.0, 2.0);
+    }
+
+    if (between(6.0)) {
+        v = easeElasticOut(animation.pow);
+        p = p + vec2(r * cos(v * 2.0 * PI), r * sin(v * 2.0 * PI));
+        object.color = mix(WHITE, GREEN, v);
+        object.distance = roundrect(p, vec2(50.0), 5.0, 2.0);
+    }
+    
+    if (between(2.0)) {
+        v = easeBounceOut(animation.pow);
+        p += vec2(mix(r, -r, v), 0.0);
+        object.color = mix(GREEN, YELLOW, v);
+        object.distance = polygon(p, 50.0, 4, 2.0);
+    }
+
+    // object.distance = smoothstep(0.9 - rx, 0.9 + rx, object.distance);
+    // object.distance = circle(p, 30.0 + 20.0 * v);
+}
+
+void main() {
+    vec3 color = BLACK;
+    
+    color = mix(color, GREEN, grid(50.0));
+
+    animate(0.0);
+
+    color = mix(color, 
+        object.color, object.distance
+    );
+
+    color = mix(color, 
+        MAGENTA, roundrect((pos(-125.0, -125.0)) * rotate2d(-u_time), vec2(100.0, 100.0), 10.0)
+    );
+
+    color = mix(color, 
+        MAGENTA, roundrect((pos(125.0, -125.0)) * rotate2d(u_time), vec2(100.0, 100.0), 10.0, 1.0)
+    );
+
+    color = mix(color, 
+        MAGENTA, circle(pos(-125.0, 125.0), 30.0)
+    );
+
+    color = mix(color, 
+        MAGENTA, circle(pos(125.0, 125.0), 30.0, 1.0)
+    );
+  
+    color = mix(color, 
+        MAGENTA, rect(pos(-125.0, 225.0), 
+        vec2(90.0 + sin(u_time) * 60.0, 90.0 + cos(u_time) * 60.0))
+    );
+
+    color = mix(color, 
+        MAGENTA, rect(pos(125.0, 225.0), 
+        vec2(90.0 + cos(u_time) * 60.0, 90.0 + sin(u_time) * 60.0), 1.0)
+    );
+
+    color = mix(color, 
+        YELLOW, polygon(pos(-125.0, -225.0), 30.0, 3)
+    );
+
+    color = mix(color, 
+        YELLOW, polygon(pos(125.0, -225.0), 30.0, 3, 4.0)
+    );
+
+    color = mix(color, 
+        YELLOW, line(pos(50.0 * cos(u_time), 0.0), pos(150.0 * sin(u_time), 150.0), 5.0)
+    );
+
+    color = mix(color, 
+        YELLOW, line(pos(-50.0 * cos(u_time), 0.0), pos(-150.0 * sin(u_time), 150.0), 5.0)
+    );
+
+    /*
+    vec2 a = pos(50.0, 0.0) * vec2(cos(st.x + u_time), sin(st.y + u_time));
+    vec2 b = pos(150.0, 150.0) * vec2(sin(st.x * u_time), cos(st.y - u_time));
+    color = mix(color, 
+        BLUE, line(a, b, 3.0)
+    );
+    */
+
+
+    vec4 tx = texture2D(u_texture_0, uv);
+
+    color = mix(color, tx.rgb, 
+        rect(st, vec2(100.0))
+    );
+
+
+    float c = 0.0; // cos(u_time * 60.0) * 0.05;
+
+    gl_FragColor = vec4(color + c * length(st), 1.0);
 }
